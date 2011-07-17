97356022322a77599f586023ff328bc2464088ec
Pawel Pabich <pawel@pabich.eu>
2011-07-17 18:29:30 +1000

    intial drop

---
+++ ReliableE2ETestsWithSelenium/ReliableE2ETestsWithSelenium.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReliableE2ETestsWithSelenium.Tests
+{
+    public class Class1
+    {
+    }
+}
